0b5810de67d5f9de6618f1c2073051f9ee82210e
lucas <lucasluz1710@gmail.com>
2024-02-15 11:45:43 -0300

    default layout
    Adds Default Layout using the react-router-dom library for route control and styled-components for styling. This commit also includes the installation of the react-router-dom and styled-components libraries. The 'layout' folder have been created, containing the necessary files and relevant import. The default layout is not complete yet!

---
+++ web/src/layout/index.tsx
@@ -0,0 +1,17 @@
+import { LayoutContainer, LayoutContent } from "./styles";
+import { Outlet } from "react-router-dom"; 
+
+
+export function Layout() {
+
+  return (
+    <LayoutContainer>
+      <LayoutContent>
+        
+
+        <Outlet />
+
+      </LayoutContent>
+    </LayoutContainer>
+  );
+};
---
+++ web/src/layout/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+
+export const LayoutContainer = styled.div``;
+
+export const LayoutContent = styled.div``;
